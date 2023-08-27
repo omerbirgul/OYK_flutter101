cb58ea8f9ad3290c86322ba3bf7a8f703ee46fba
Omer Faruk Birgul <omerbirgul1@hotmail.com>
2023-08-27 12:31:41 +0300

    OYK flutter day1 chapter2

--- Day_1/learn.dart
+++ Day_1/learn.dart
@@ -1,12 +1,8 @@
 void main(List<String> args){
-  String name = "Omer";
-  int num = 1;
-  print("hello world ${num}");
-  print(name);
 
-  try {
-    print(int.parse(name));
-  } catch (e) {
-    print("this is not correct");  
-  }
+  List<String> namelist = ["pardus", "casper", "mono"];
+  print(namelist.length);
+
+  Map user = {"name": "Omer", "age": 1};
+  print(user["name"]);
 }
