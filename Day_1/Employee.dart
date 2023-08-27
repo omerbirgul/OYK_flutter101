0f924de32931d94f8e059d07ae5f4a31df6df2eb
Omer Faruk Birgul <omerbirgul1@hotmail.com>
2023-08-27 15:56:22 +0300

    OYK flutter day1 chapter4

---
+++ Day_1/Employee.dart
@@ -0,0 +1,14 @@
+class Employee {
+  final String name;
+  final double salary;
+  final String department;
+
+Employee(this.name ,this.salary, this.department){
+
+
+}
+
+void showDepartment(){
+  print("deprartment is $department");
+}
+}
--- Day_1/learn.dart
+++ Day_1/learn.dart
@@ -1,22 +1,13 @@
-void main(List<String> args){
+import 'Employee.dart';
 
-  // showNumber(name: "omer",id: 99999, age: 25);
-  
-  print(calculateDiscount(25));
+void main(){
+
+
+  Employee newEmployee = Employee("omer", 2000, "engineering");
+
+  print("employee name: ${newEmployee.name}, employee salary: ${newEmployee.salary}, employee department: ${newEmployee.department}");
+
+  print("hello wordl");
 }
 
-void showNumber({
-  required int id,
-  String? name,
-  int? age,
-  List<String>? users,
-})
-    {
-      print(" your name is $name, your age is $age, your id is $id");
-    }
 
- double discount = 0.1; 
-    double calculateDiscount(double price){
-      double calcDis =  price * discount;
-    return calcDis;
-    }
