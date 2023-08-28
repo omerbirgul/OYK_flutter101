03702b3122001c55b4b0d02d4895e2620fe24f9b
Omer Faruk Birgul <omerbirgul1@hotmail.com>
2023-08-28 15:37:53 +0300

    OYK flutter day2 chapter1

---
+++ Day_2/flutter_application_1/lib/main.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+void main(){
+  runApp(StateApp());
+}
+
+class StateApp extends StatelessWidget{
+  @override
+  Widget build(BuildContext context){
+    throw UnimplementedError();
+  }
+
+}
+  class MyWidget extends StatelessWidget{
+    const MyWidget({super.key});
+
+    @override
+    Widget build(BuildContext context){
+      return const Placeholder();
+    }
+  }
+    
+   
