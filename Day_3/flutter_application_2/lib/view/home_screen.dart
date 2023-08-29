0f8548fcee209fc3847c3af5720e6291264747e7
Omer Faruk Birgul <omerbirgul1@hotmail.com>
2023-08-29 10:57:10 +0300

    OYK flutter day3 chapter2

--- Day_3/flutter_application_2/lib/view/home_screen.dart
+++ Day_3/flutter_application_2/lib/view/home_screen.dart
@@ -1,17 +1,28 @@
 import 'package:flutter/material.dart';
-
 import 'detail_screen.dart';
 
-class HomeScreen extends StatelessWidget {
-  final String title = "Name";
+class HomeScreen extends StatefulWidget {
 
   HomeScreen({super.key});
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  final String title = "Name";
+
+  bool isRead = false;
+
+// *********************************************************
+// *********************************************************
+
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text("Users"),
+        title: const Text("User"),
         centerTitle: true,
       ),
       body: ListView.builder(
@@ -20,6 +31,10 @@ class HomeScreen extends StatelessWidget {
       ),
     );
   }
+
+// *********************************************************
+// *********************************************************
+
 
   Widget _buildUserCard(BuildContext context, int index) {
     return Card(
@@ -38,25 +53,41 @@ class HomeScreen extends StatelessWidget {
         subtitle: const Text(
           "Surname",
         ),
+        
         trailing: IconButton(
           icon: const Icon(
             Icons.arrow_right,
             size: 40,
           ),
           onPressed: () async {
-            bool isTrue = await Navigator.of(context).push(
-              MaterialPageRoute(
-                builder: (context) {
-                  return DetailScreen(
-                    appBarTitle: "$title ${(index + 1)}",
-                  );
-                },
-              ),
-            );
-            print("TRUE MU:  $isTrue");
+            navigateToDetail(context, index);
+            
+            setState((){
+
+              isRead = true;
+            });
+            
           },
         ),
+        tileColor: isRead ? Colors.white : Colors.deepPurple.shade100,
       ),
     );
+  }
+
+
+// *********************************************************
+// *********************************************************
+
+  Future<void> navigateToDetail(BuildContext context, int index) async {
+    bool isTrue = await Navigator.of(context).push(
+      MaterialPageRoute(
+        builder: (context) {
+          return DetailScreen(
+            appBarTitle: "$title ${(index + 1)}",
+          );
+        },
+      ),
+    );
+    print("TRUE MU:  $isTrue");
   }
 }
