a062e621c15e9928fee1f4cf5a7bcf9ead38868b
Omer Faruk Birgul <omerbirgul1@hotmail.com>
2023-08-30 19:25:44 +0300

    OYK flutter day4 chapter3 movieApp

--- Day_4/movieapp/lib/components/movieCard.dart
+++ Day_4/movieapp/lib/components/movieCard.dart
@@ -11,6 +11,8 @@ class MovieCard extends StatefulWidget {
 }
 
 class _MovieCardState extends State<MovieCard> {
+  String snackBarText = "";
+
   @override
   Widget build(BuildContext context) {
     return Padding(
@@ -19,20 +21,35 @@ class _MovieCardState extends State<MovieCard> {
         mainAxisAlignment: MainAxisAlignment.spaceBetween,
         children: [
           Image.asset(widget.selectedMovie.imagePath,
-              width: MediaQuery.of(context).size.width * 0.4
-              ),
+              width: MediaQuery.of(context).size.width * 0.4,
+              height: MediaQuery.of(context).size.height * 0.15),
           Text(widget.selectedMovie.description),
           IconButton(
             onPressed: () {
               setState(() {
-              widget.selectedMovie.isFavorite =
-                  !widget.selectedMovie.isFavorite;
+                widget.selectedMovie.isFavorite =
+                    !widget.selectedMovie.isFavorite;
               });
+
+              if (widget.selectedMovie.isFavorite) {
+                snackBarText = "Movie added to favorites";
+              } else {
+                snackBarText = "Movie removed from favorites";
+              }
+
+              final snackBar = SnackBar(
+                content: Text(snackBarText),
+                action: SnackBarAction(
+                  label: 'Undo',
+                  onPressed: () {
+                    // Some code to undo the change.
+                  },
+                ),
+              );
+              ScaffoldMessenger.of(context).showSnackBar(snackBar);
             },
             icon: Icon(Icons.favorite),
-            color: widget.selectedMovie.isFavorite
-                ? Colors.red
-                : Colors.blue,
+            color: widget.selectedMovie.isFavorite ? Colors.red : Colors.blue,
           ),
         ],
       ),
